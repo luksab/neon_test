13c7c17d57fb20d357163f66b49334d157b9b5f3
luksab <emma@sabatschus.de>
2025-09-28 19:08:36 +0200

    remove unneeded step in double_array_sisd_opt_iter

--- src/opt.rs
+++ src/opt.rs
@@ -27,7 +27,6 @@ pub fn double_array_sisd_opt_iter(array: &[u8]) -> Vec<u8> {
         .iter()
         .flat_map(|&x| {
             let num: u16 = x as u16;
-            let num = num & 0b0000_0000_1111_1111 | (num & 0b1111_1111_0000_0000) << 8;
             let num = num & 0b0000_1111_0000_1111 | (num & 0b1111_0000_1111_0000) << 4;
             let num = num & 0b0011_0011_0011_0011 | (num & 0b1100_1100_1100_1100) << 2;
             let num = num & 0b0101_0101_0101_0101 | (num & 0b1010_1010_1010_1010) << 1;
